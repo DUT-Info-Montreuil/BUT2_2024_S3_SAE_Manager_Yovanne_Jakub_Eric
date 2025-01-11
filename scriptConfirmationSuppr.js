d96e73c4b635185155b99b86cebbebbdc230f276
Mazur123456 <jakubmazur1356@gmail.com>
2025-01-12 00:30:05 +0100

    popup pour supprimer les document deposer (etudiant)

--- scriptConfirmationSuppr.js
+++ scriptConfirmationSuppr.js
@@ -1,4 +1,4 @@
-// Fonction JavaScript pour afficher une pop-up de confirmation
+// Fonction JavaScript pour afficher une pop-up et demande la confiramtion avant de supprimer
 function confirmationSupprimer() {
     return confirm("Êtes-vous sûr de vouloir supprimer ce document ?");
 }
