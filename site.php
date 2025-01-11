86dce3c65dee04acdfdfbc1dad564e0d9599d4d5
Yovanne <yovanne.ponnou@gmail.com>
2025-01-11 20:50:01 +0100

    renommage de la class style

--- site.php
+++ site.php
@@ -7,12 +7,12 @@ include "headerController.php";
 <head>
     <meta charset="UTF-8">
     <title>SAE Manager</title>
-    <link href="styleSite.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
-<header class="bg-primary text-white py-3 mb-4 d-flex align-items-center justify-content-between">
+<header class="bg-primary text-white mb-4 d-flex align-items-center justify-content-between">
     <a href="index.php" class="d-flex align-items-center">
         <img src="assets/home.png" alt="Home" width="30" height="30">
     </a>
