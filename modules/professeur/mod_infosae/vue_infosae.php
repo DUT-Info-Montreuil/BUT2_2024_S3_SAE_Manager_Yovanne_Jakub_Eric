66c51fdaba99dff381460d91ba32eddabcbf7937
Mazur123456 <jakubmazur1356@gmail.com>
2025-01-12 01:18:04 +0100

    popup de confirmation pour supprimer une sae (profeseur)

--- modules/professeur/mod_infosae/vue_infosae.php
+++ modules/professeur/mod_infosae/vue_infosae.php
@@ -90,9 +90,10 @@ class VueInfoSae extends VueGenerique
 
                 <button type="submit" class="btn btn-primary">Mettre à jour</button>
             </form>
-            <form action="index.php?module=infosae&action=supprimerSAE" method="post">
+            <form action="index.php?module=infosae&action=supprimerSAE" method="post" onsubmit="return confirmationSupprimer();">
                 <button type="submit" class="btn btn-danger">Supprimer la SAE</button>
             </form>
+            <script src="scriptConfirmationSuppr.js"></script>
         </div>
         <?php
     }
